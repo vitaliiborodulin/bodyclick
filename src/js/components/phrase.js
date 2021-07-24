98c5e24841571914d127b0c3cea73fbb79821e95
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2021-07-24 20:07:18 +0300

    updates

--- src/js/components/phrase.js
+++ src/js/components/phrase.js
@@ -1,19 +1,44 @@
 var set = ["вазндан халос булишни",
-"чиройли қоматга",
-"янги овқатланиш",
-"ўзига бўлган ишончга"];
+  "чиройли қоматга",
+  "янги овқатланиш",
+  "ўзига бўлган ишончга"];
+
+var setRu = [
+  "скинуть лишние килограммы",
+  "подтянутое тело",
+  "новые пищевые привычки",
+  "уверенность в себе"
+];
+
 var utObject = document.getElementById("ph");
+var utObjectRu = document.getElementById("ph-ru");
 
-var m=0;
+var m = 0;
 
-function change (m){
-    utObject.innerText = set[m];
+function change(m) {
+  utObject.innerText = set[m];
 }
-//setInterval (change, 5000);
-setInterval(function(){
-  change(m);
-  m++;
-  if (m >= set.length) {
-    m = 0;
-  }
-}, 3000);
+function changeRu(m) {
+  utObjectRu.innerText = setRu[m];
+}
+
+if (utObject) {
+  setInterval(function () {
+    change(m);
+    m++;
+    if (m >= set.length) {
+      m = 0;
+    }
+  }, 3000);
+}
+
+if (utObjectRu) {
+  setInterval(function () {
+    changeRu(m);
+    m++;
+    if (m >= setRu.length) {
+      m = 0;
+    }
+  }, 3000);
+}
+
